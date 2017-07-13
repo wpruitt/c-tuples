bcd820be7b11006231b48bd4709bcc7e23098881
Willie Pruitt <w_pruitt@hotmail.com>
2017-07-12 20:33:10 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         List<(string, double, int)> transactions = new List<(string, double, int)>();  
+         transactions.Add(("Shoelace", 2.50, 10));
+         transactions.Add(("Buttons", 0.50, 8));
+         transactions.Add(("Hat", 15.50, 4));
+         transactions.Add(("T-shirt", 9.50, 5));
+         transactions.Add(("Glasses", 22.10, 3));
+         int totalSold = 0;
+         double totalAmount = 0;
+         foreach ((string product, double amount, int quantity) t in transactions)
+         {
+             totalSold += t.quantity;
+             totalAmount += t.amount;
+         }
+         Console.WriteLine("Items sold today: " + totalSold);
+         Console.WriteLine("Total revenue: " + totalAmount);
+        }
+    }
+}
